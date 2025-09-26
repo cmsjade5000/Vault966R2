c542cb8e39ac4342cef2b8ce4afcbfcda6defa5f
cmsjade5000 <cory.stoner@icloud.com>
2025-09-26 15:49:10 -0400

    Escape wildcard characters in movie title search

--- api/services/movie_filters.py
+++ api/services/movie_filters.py
@@ -101,9 +101,8 @@ def _to_like_pattern(term: str) -> str:
 
 def apply_filters(query: Query, params: MovieFilterParams) -> Query:
     if params.q:
-        query = query.filter(
-            Movie.title.ilike(_to_like_pattern(params.q), escape="\\")
-        )
+        pattern = _to_like_pattern(params.q)
+        query = query.filter(Movie.title.ilike(pattern, escape="\\"))
     for genre_name in params.genres:
         search_terms = search_terms_for_label(genre_name)
         if search_terms:
