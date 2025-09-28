3c37ea924fe9f767661858d1b19d1566dcad4f8a
Cory Stoner <Cory.Stoner@icloud.com>
2025-09-27 22:13:22 -0400

    alembic: guard person unique constraint and indexes (idempotent)

---
+++ alembic/versions/82242007879d_merge_heads.py
@@ -0,0 +1,24 @@
+"""Merge heads
+
+Revision ID: 82242007879d
+Revises: 202410101204, ca876fb0a006
+Create Date: 2025-09-28 01:51:57.602824
+
+"""
+
+from typing import Sequence, Union
+
+
+# revision identifiers, used by Alembic.
+revision: str = "82242007879d"
+down_revision: Union[str, None] = ("202410101204", "ca876fb0a006")
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
